115572b49deaa13f88cc183bd378a485a9f776f6
Yunus Naseri <mohammadyunusn@vt.edu>
2025-07-17 12:22:42 -0400

    Revert "updated the team"
    This reverts commit d7632fef21b3ba797bfc17b307d9ebc652fb8321.

--- exportable/server/middleware-build-manifest.js
+++ exportable/server/middleware-build-manifest.js
@@ -25,15 +25,10 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/live-data": [
+    "/chatbot": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/live-data.js"
-    ],
-    "/team": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/team.js"
+      "static/chunks/pages/chatbot.js"
     ]
   },
   "ampFirstPages": []
